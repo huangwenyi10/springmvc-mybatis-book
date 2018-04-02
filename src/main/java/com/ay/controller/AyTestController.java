23ffb3bc8a1033eb715fb65cdbd3722f4b590865
Ay <wenyi.huang@e-vada.com>
2018-04-02 16:16:42 +0800

    ### book

---
+++ src/main/java/com/ay/controller/AyTestController.java
@@ -0,0 +1,21 @@
+package com.ay.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *@author Ay
+ * @date 2018/04/02
+ */
+@RestController
+@RequestMapping("/test")
+public class AyTestController {
+
+
+    @GetMapping("/sayHello")
+    public void sayHello(){
+        System.out.println("hello ay!");
+    }
+
+}
---
+++ src/main/test/com/ay/test/SpringTest.java
@@ -0,0 +1,26 @@
+package com.ay.test;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+/**
+ * @author Ay
+ * @date 2018/04/02
+ */
+@Service
+public class SpringTest {
+
+    @Test
+    public void testSpring(){
+        ApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        SpringTest springTest = (SpringTest) applicationContext.getBean("springTest");
+        springTest.sayHello();
+
+    }
+
+    public void sayHello(){
+        System.out.println("hello ay");
+    }
+
+}
