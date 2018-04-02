2af98e00328cbe3a07280fb44c2af11bdcee899d
Ay <wenyi.huang@e-vada.com>
2018-04-02 19:33:39 +0800

    ### book

--- src/main/java/com/ay/controller/AyTestController.java
+++ src/main/java/com/ay/controller/AyTestController.java
@@ -1,21 +1,19 @@
 package com.ay.controller;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 /**
  *@author Ay
  * @date 2018/04/02
  */
-@RestController
-@RequestMapping("/test")
+@Controller
+@RequestMapping(value = "/test")
 public class AyTestController {
 
-
     @GetMapping("/sayHello")
-    public void sayHello(){
-        System.out.println("hello ay!");
+    public String sayHello(Model model){
+        return "hello";
     }
-
 }
